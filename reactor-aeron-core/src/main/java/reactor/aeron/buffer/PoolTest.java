afbdf9e599fd65015146fc6a51f18fd9a5fba420
segabriel <sergiy.gabriel@jivygroup.com>
2019-02-09 20:32:32 +0200

    Fixed working with indexes

--- reactor-aeron-core/src/main/java/reactor/aeron/buffer/PoolTest.java
+++ reactor-aeron-core/src/main/java/reactor/aeron/buffer/PoolTest.java
@@ -2,22 +2,24 @@ package reactor.aeron.buffer;
 
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
+import java.util.Random;
 import reactor.core.publisher.Mono;
 
 public class PoolTest {
 
   public static void main(String[] args) throws InterruptedException {
-
     String msg = "hello";
-    int length = msg.getBytes(StandardCharsets.UTF_8).length;
 
-    BufferPool pool = new BufferPool(1024);
+    BufferPool pool = new BufferPool(10240);
 
-    for (int i = 0; i < 10000; i++) {
-      Thread.sleep(1);
-      BufferSlice slice = pool.allocate(233);
+    Random random = new Random();
+
+    for (int i = 0; i < 1000000; i++) {
+      Thread.sleep(50);
+      BufferSlice slice = pool.allocate(23 + random.nextInt(500));
       slice.putStringUtf8(0, msg);
-      Mono.delay(Duration.ofMillis(100)).doOnSuccess($ -> slice.release()).subscribe();
+//      slice.print();
+      Mono.delay(Duration.ofMillis(20)).doOnSuccess($ -> slice.release()).subscribe();
     }
   }
 }
