b134c97c017baae8b71b5bbb80f63d47dd15dc95
Artem Vysochyn <artem.vysochyn@gmail.com>
2019-11-04 13:35:58 +0200

    Set mtu-length to 65504

--- reactor-aeron-benchmarks/scripts/pure/mdc-ping.sh
+++ reactor-aeron-benchmarks/scripts/pure/mdc-ping.sh
@@ -16,4 +16,5 @@ java \
     -Dreactor.aeron.sample.idle.strategy=yielding \
     -Dreactor.aeron.sample.frameCountLimit=16384 \
     -Dreactor.aeron.sample.messageLength=32 \
+    -Daeron.mtu.length=65504 \
     ${JVM_OPTS} reactor.aeron.pure.MdcPing
--- reactor-aeron-benchmarks/scripts/pure/mdc-pong.sh
+++ reactor-aeron-benchmarks/scripts/pure/mdc-pong.sh
@@ -15,5 +15,5 @@ java \
     -Dreactor.aeron.sample.exclusive.publications=true \
     -Dreactor.aeron.sample.idle.strategy=yielding \
     -Dreactor.aeron.sample.frameCountLimit=16384 \
-    -Daeron.mtu.length=16k \
+    -Daeron.mtu.length=65504 \
     ${JVM_OPTS} reactor.aeron.pure.MdcPong
